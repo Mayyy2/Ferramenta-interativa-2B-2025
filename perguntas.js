d985bc9dd3d19d3ae0aa6b3d24f2c794c0648be8
Mayyy2 <dolce.mayara@escola.pr.gov.br>
2025-09-17 16:25:56 -0300

    17/09

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,12 @@ criacartao(
     'O que é JavaScript?',
     'O JavaScript é uma linguagem de programação'
 )
+
+criacartao(
+    'Fisica', 
+    'Qual é a primeira lei de Newton?',
+    'Lei da Inércia'
+)
+
+
+
