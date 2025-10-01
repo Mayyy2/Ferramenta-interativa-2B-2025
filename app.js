b05b43268ff660bfa2a77d7c74931a8e4707eafa
Mayyy2 <dolce.mayara@escola.pr.gov.br>
2025-10-01 15:34:14 -0300

    01/10

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function criaCartao(categoria, pergunta, resposta) {
    //17-09-25//
     cartao.innerHTML = `
         <div class="cartao__conteudo">
-                <h3>${cate}</h3>
+                <h3>${categoria}</h3>
                 <div class="cartao__conteudo__pergunta">
                       <p>${pergunta}</p> 
                 </div>
